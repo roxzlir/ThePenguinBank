608fcd5c1fef3138c915c6dd96219fd2d91a7dc8
roxzlir <emil.ch.nordin@gmail.com>
2023-11-15 14:07:20 +0100

    Added abstract Account class complete with properties and constructor.

---
+++ Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePenguinBank
+{
+    abstract class Account
+    {
+        public double CustomerID { get; set; }
+        public int AccountID { get; set; }
+        public Account(double customerID, int accountID)
+        {
+            CustomerID = customerID;
+            AccountID = accountID;
+        }
+    }
+}
