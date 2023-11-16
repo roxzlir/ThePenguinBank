2c3584a2c861c102752133150efa038e985b1b72
roxzlir <emil.ch.nordin@gmail.com>
2023-11-16 11:27:52 +0100

    Added Checking class complete with properties and constructor.

---
+++ Checking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePenguinBank
+{
+    internal class Checking : Account
+    {
+        public double Balance { get; set; }
+        public Checking(double customerID, int accountID, double balance) : base(customerID, accountID)
+        {
+            CustomerID = customerID;
+            AccountID = accountID;
+            Balance = balance;
+        }
+    }
+}
