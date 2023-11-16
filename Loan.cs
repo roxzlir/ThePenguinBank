0811ff70db6975181844a7e443c2dc3eacf9f027
roxzlir <emil.ch.nordin@gmail.com>
2023-11-16 23:34:56 +0100

    Have just copied in this code from Caroline since she had problem with branch.

---
+++ Loan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePenguinBank
+{
+    internal class Loan : Account
+    {
+        public double Amount { get; set; }
+        public double InterestRate { get; set; }
+        public bool Approved { get; set; }
+
+        public Loan(double customerID, int accountID, double amount, double interestRate, bool approved) : base(customerID, accountID)
+        {
+            Amount = amount;
+            InterestRate = interestRate;
+            Approved = approved;
+        }
+    }
+}
