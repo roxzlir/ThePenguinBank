6ba72ca84563d8033c95264f919346571a281cb9
roxzlir <emil.ch.nordin@gmail.com>
2023-11-16 11:02:21 +0100

    Added first version of method for login function, TryLogin. (Will make changes according to class name's later)

--- Program.cs
+++ Program.cs
@@ -28,6 +28,42 @@
             }
             return userInput;
         }
+        static string TryLogin(int securityNumber, int userPassword)
+        {
+            int maxAttempts = 3;
+
+            int adminUsername = 0000; int adminPassword = 9999;
+
+
+
+            for (int attempt = 0; attempt <= maxAttempts; attempt++)
+            {
+                Console.Write($"Ange användarnamn: ");
+                int username = GetInputNumber();
+
+                Console.Write($"Ange lösenord: ");
+                int password = GetInputNumber();
+
+                if (username == adminUsername && password == adminPassword)
+                {
+                    return "adminPassed";
+                    break;
+
+
+                }
+                if (username == securityNumber && password == userPassword)
+                {
+                    return "customerPassed";
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Inloggning misslyckades, antingen fel användarnamn eller fel lösenord. Försök {attempt}/{maxAttempts}.\n");
+                }
+            }
+
+            return "LoginFailed";
+        }
     }
 
 }
