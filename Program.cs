3dea114b8bf8a873fc95e110f66681f4a326080e
roxzlir <emil.ch.nordin@gmail.com>
2023-11-16 10:58:40 +0100

    Added method for getting a int from user.

--- Program.cs
+++ Program.cs
@@ -6,5 +6,28 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+
+        public static int GetInputNumber()
+        {
+            int userInput;
+            while (true)
+            {
+
+                string inputNumber = Console.ReadLine();
+
+                if (int.TryParse(inputNumber, out userInput))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Du gjorde en felaktig inmatning!");
+                }
+
+            }
+            return userInput;
+        }
     }
+
 }
