4a8ce55beed3359a52161bd878eaf30f7d8d6786
roxzlir <emil.ch.nordin@gmail.com>
2023-11-21 10:23:59 +0100

    Added method in Admin to collect a input number from user. Will use this in method for creating new customer obj.

--- Admin.cs
+++ Admin.cs
@@ -10,4 +10,23 @@ internal class Admin : Account
 
     public string NewCustomer { get; set; }
     public string UserName { get; set; }
+
+    public static double GetInputNumber()
+    {
+        double userInput;
+        while (true)
+        {
+            string inputNumber = Console.ReadLine();
+            if (double.TryParse(inputNumber, out userInput))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("You made an incorrect entry, please enter integers only.");
+            }
+        }
+        return userInput;
+    }
+   
 }
