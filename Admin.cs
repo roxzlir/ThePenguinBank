292a7fc921810a07b528efc06c36261429d8c595
uthawong <burruthawong@gmail.com>
2023-11-21 14:26:38 +0100

    Added exchange method

--- Admin.cs
+++ Admin.cs
@@ -10,4 +10,15 @@ internal class Admin : Account
 
     public string NewCustomer { get; set; }
     public string UserName { get; set; }
+
+
+    static void SEKToUSD()
+    {
+        Console.Write("Convert an amount in Swedish Krona (SEK) to US Dollar (USD): ");
+        decimal SEK = Convert.ToDecimal(Console.ReadLine());
+
+        decimal USD = SEK / 10.4275286757M;
+        Console.WriteLine(SEK + " SEK converts to " + Math.Round(USD) + " USD.");
+    }
 }
+
